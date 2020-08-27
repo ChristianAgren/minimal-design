ee2cc0d9c8d1959bb18c48279515eb2173de54bb
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-08-28 00:17:16 +0200

    small tweaks

--- src/components/title.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-    font-family: 'Alata', sans-serif;
-    font-size: ${props => props.size && props.size};
-    color: ${props => props.color && props.color};
-`
-
-export default Title
